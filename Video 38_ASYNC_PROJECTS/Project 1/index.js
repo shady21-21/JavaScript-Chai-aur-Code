3445b0ee5ab0e01333e3f03e020106e63c5848bb
Debapriyo Das <debapriyo00@gmail.com>
2024-01-15 14:48:11 +0530

    Project 1 Updated and Some Concept also Updated

---
+++ Video 38_ASYNC_PROJECTS/Project 1/index.js
@@ -0,0 +1,33 @@
+
+//generate random color.
+const randomColorGene = function(){
+    const hex = '0123456789ABCDEF'
+    let color = '#'
+    for(let i = 0;i<6;i++){
+        color += hex[Math.floor(Math.random() * 16)]
+    }
+    return color;
+};
+// console.log(randomColorGene())
+
+//startChanging Color Function..
+let intervalId
+const startChangingColor = function(){
+    if(!intervalId){
+        intervalId = setInterval(changeBgColor,1000)
+    }
+    function changeBgColor(){
+      document.body.style.backgroundColor = randomColorGene()
+    }
+};
+
+const stopChangingColor = function(){
+    clearInterval(intervalId)
+    intervalId = null
+};
+
+document.querySelector('#start').addEventListener
+('click', startChangingColor)
+
+document.querySelector('#stop').addEventListener
+('click', stopChangingColor)
